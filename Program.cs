f84f022e996ef3419e8ebc1c07496a889180641b
Aditya Khatry <adityak@lims1.com>
2023-06-01 17:04:29 +1000

    Basic functionality added.

--- Program.cs
+++ Program.cs
@@ -136,8 +136,58 @@ public class cardHolder
 
             catch
             {
-                Console.WriteLine("Card not recognized. Please try again")
+                Console.WriteLine("Card not recognized. Please try again");
             }
         }
+
+        Console.WriteLine("Please enter your pin: ");
+        int userPin = 0;
+        while (true)
+        {
+            try
+            {
+                userPin = int.Parse(Console.ReadLine());
+                if (currentUser.getPin() == userPin)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect pin. Please try again");
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Incorrect pin. Please try again");
+            }
+        }
+
+        Console.WriteLine("Welcome " + currentUser.getFirstName());
+        int option = 1;
+        do
+        {
+            printOptions();
+            try
+            {
+                option = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+
+            }
+
+            if (option == 1) { deposit(currentUser); }
+            else if (option == 2) { withdraw(currentUser); }
+            else if (option == 3) { balance(currentUser); }
+            else if (option == 4) { break; }
+            else { option = 0; }
+
+        }
+
+        while (option != 4);
+        Console.WriteLine("Thank you! Have a nice day.");
+
+
     }
 }
